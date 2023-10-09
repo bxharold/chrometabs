8ea84758079ac84bfec92ff1f23b2bce4e84e3fc
bxharold <hfinzz@gmail.com>
2023-10-09 15:20:23 -0700

    changed name from mytabs.sql to make_mytabs.sql

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
+--    project chrometabs:   make_mytabs.sql
+--    puts a copy of all Chrome tabs for all windows into a database
+--    companion to flask app chrometabs.py
+--       renamed mytabs.sql 10/5/2023 after "pbpaste > mytabs.sql" oopsie
+--    1. use TabCopy chrome extension:  copy all tabs
+--    2. ipbpaste > mytabs.csv   (or cat > mytabs.csv [ctrlC])
+--       maybe hand-scrub a bit.... e.g., there was an unmatched "
+--    3. import into mytabs sqlite database mytabs.db, table mytabs:
+--       code is in make_mytabs.sql, exec'd using the "-init" argument
+--       remove (nn) from title; e.g., change title "(19) CS50P  to "CS50P
+--       .import '|sed "s/^.([0-9][0-9]*) /\"/" mytabs.csv' mytabs
+--       use SQL to remove gmail, amazon tbs, etc
+--    pbpaste > mytabs.csv
+--    sqlite3 mytabs.db -init make_mytabs.sql
+--    python3 chrometabs.py
+
+.mode csv
+
+
 # Project chrometabs: 
-# A concise way to view my open chrome windows and tabs.
+## Puts a copy of all Chrome tabs for all windows into a database
+## A concise way to view my open chrome windows and tabs.
 - ... and yet another flask sample app
 - ... and another exercise in creating a git repo
+- ... renamed mytabs.sql 10/5/2023 after "pbpaste > mytabs.sql" oopsie
+## Companion apps: make_mytabs.sql, flask app chrometabs.py
 
-# Usage:  
-##  >> Data Gathering Step:
+# *** Usage:  ***
+## >> Data Gathering Step:
 ### 1. use TabCopy chrome extension:  copy all tabs
-### 2. Paste date into a .csv file
-## pbpaste > mytabs.csv  (or cat > mytabs.csv [ctrlC])   
-    maybe hand-scrub a bit.... there was an unmatched "
+### 2. Paste date into a .csv file: (the names are hardcoded.)
+# pbpaste > mytabs.csv  <!-- or cat > mytabs.csv [ctrlC])-->  
 ### 3. import into mytabs sqlite database mytabs.db, table mytabs.
-## sqlite3 mytabs.db -init mytabs.sql
-
-  code is in mytabs.sql, exec'd using the "-init" argument
-
+  <!-- code is in mytabs.sql, exec'd using the "-init" argument. -->
+# sqlite3 mytabs.db -init mytabs.sql
   1. this removes (nn) from title; e.g., changes title "(19) CS50P...  to "CS50P...
-
-  1. .import '|sed "s/^.([0-9][0-9]*) /\"/" mytabs.csv' mytabs
-    
+  1. .import '|sed "s/^.([0-9][0-9]*) /\"/" mytabs.csv' mytabs 
   1. extra adhoc inserts... because.  just because.
 
 ## >> Display Step:
--- chrometabs.py
+++ chrometabs.py
@@ -1,8 +1,6 @@
 #!/usr/local/bin/python3   # HiMac2:~/vscode/yanother_flask_app/yafa.py  9/25/2023
 ### exploring the difference between jsonify(data) and json.dumps(data)
-### uses birthday as model for the database code and jinja2 template looping
-### The idea was only to display mytabs query result using both jasonify and jdomps
-### ... but this sidetrack is turning into a useful tab-tracker.
+### used birthday as model for the database code and jinja2 template looping
 
 # lsof -iTCP -sTCP:LISTEN -n -P
 
@@ -28,12 +26,7 @@ def returnjson():
     print("jsonifydata", type(jsonifydata), jsonifydata)
     # jsonifydata <class 'flask.wrappers.Response'> <Response 40 bytes [200 OK]>
     return jsonifydata     # 4 lines on a black page
-"""
-{
-  "day": 25,
-  "month": "September"
-}
-"""
+
 @app.route('/returndumps', methods = ['GET'])
 def returndumps():
     data = { "month" : "September",  "day" : 25, }
@@ -41,9 +34,7 @@ def returndumps():
     print("jsondumpsdata", type(jsondumpsdata), jsondumpsdata)
     # jsondumpsdata <class 'str'> {"month": "September", "day": 25}
     return jsondumpsdata    # 1 line on a white page
-"""
-{"month": "September", "day": 25}
-"""
+
 @app.route('/', methods = ['GET'])
 def index():
     rows = "amusing"  # jinja2 splits this.
@@ -54,15 +45,13 @@ def index():
 
 def makeCannedQueries():
     x=[]
+    x.append("SELECT rowid, title, url FROM mytabs WHERE url LIKE '%%' ORDER BY url ")
     x.append("SELECT rowid, title, url FROM mytabs WHERE title LIKE '%%'")
     x.append("SELECT rowid, title, url FROM mytabs WHERE title LIKE '%%' ORDER BY url ")
-    x.append("SELECT rowid, title, url FROM mytabs WHERE url LIKE '%%' ORDER BY url ")
     x.append("SELECT rowid, title, url FROM mytabs WHERE title LIKE '%%' ORDER BY title ")
     x.append("SELECT rowid, title, url FROM mytabs WHERE title LIKE '%%' LIMIT 20")
-    x.append("SELECT rowid, title, substring(url,9) FROM mytabs WHERE title LIKE '%%'")
-    # substring(url,9) hack is moot now that columns are fixed-width and allow word-break
     x.append("SELECT '', '', count(*) FROM mytabs")
-    return x     # still can't understand why not    return json.dumps(x)
+    return x     # still don't understand why not    return json.dumps(x)
 
 @app.route('/query1', methods = ['GET', 'POST'])
 def query1():
